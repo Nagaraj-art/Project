c5a5715d1a4b59a4a326f484b8d16ba726b572b5
Arjunrex <arjunrex1@gmail.com>
2024-11-01 15:34:06 +0530

    Add files via upload

---
+++ fit2.js
@@ -0,0 +1,12 @@
+let menulist = document.getElementById("menulist")
+menulist.style.maxHeight = "0px"
+function togglemenu()
+{
+if(menulist.style.maxHeight == "0px")
+{
+    menulist.style.maxHeight = "300px"
+}
+else{
+    menulist.style.maxHeight = "0px"
+}
+}
